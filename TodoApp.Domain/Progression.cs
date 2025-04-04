2854c5c800e82c3456b4d5b2b11fa83544412c9b
ruben <rubenhduarte@gmail.com>
2025-04-04 10:36:51 +0200

    First commit

---
+++ TodoApp.Domain/Progression.cs
@@ -0,0 +1,15 @@
+﻿namespace TodoApp.Domain;
+
+public class Progression
+{
+    public Progression(DateTime date, 
+                       decimal percent)
+    {
+        AccionDate = date;
+        Percent = percent;
+    }
+
+    public DateTime AccionDate { get; }
+    public decimal Percent { get; }
+        
+}
---
+++ TodoApp.Domain/TodoItem.cs
@@ -0,0 +1,29 @@
+﻿namespace TodoApp.Domain;
+
+public class TodoItem
+{
+    public int Id { get; }
+    public string Title { get; }
+    public string Description { get; private set; }
+    public string Category { get; }
+    public List<Progression> Progressions { get; }
+        
+    public TodoItem(int id, 
+                    string title, 
+                    string description, 
+                    string category)
+    {
+        Id = id;
+        Title = title;
+        Description = description;
+        Category = category;
+        Progressions = new List<Progression>();
+    }
+        
+    // Se considera completado si la suma de progresiones es 100%
+    public bool IsCompleted => TotalProgress() == 100;
+        
+    public decimal TotalProgress() => Progressions.Sum(p => p.Percent);
+        
+    public void UpdateDescription(string description) => Description = description;
+}
